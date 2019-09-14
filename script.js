eed493604666f81c91827a1bffa8d713b29f4487
Raul Gonzalez <rgonz166@gmail.com>
2019-09-14 10:51:43 -0700

    initial server files, and added package.json to gitignore

---
+++ script.js
@@ -0,0 +1,12 @@
+// Dependencies
+// ===========================================================
+var express = require("express");
+
+var app = express();
+var PORT = 3000;
+
+// Data
+// ===========================================================
+app.listen(PORT, function(){
+    console.log("App listening on PORT "+PORT);
+});
